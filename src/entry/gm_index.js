ee2c990b19f5935a22f2bc5ef3f7e4db2fbe4c00
SquishedPotatoe <5186124+SquishedPotatoe@users.noreply.github.com>
2025-11-04 23:55:31 -0700

    v8.10.7
    Hot fix for querybuilder css

--- src/entry/gm_index.js
+++ src/entry/gm_index.js
@@ -2,12 +2,22 @@ import '../browser-gm';
 import '../main';
 
 (async () => {
-	const awesomeBootstrapCheckboxCss = document.createElement('link');
-	awesomeBootstrapCheckboxCss.rel = 'stylesheet';
-	awesomeBootstrapCheckboxCss.href = await GM.getResourceUrl('awesome-bootstrap-checkbox');
-	const jqueryQueryBuilderCss = document.createElement('link');
-	jqueryQueryBuilderCss.rel = 'stylesheet';
-	jqueryQueryBuilderCss.href = await GM.getResourceUrl('jquery-query-builder');
-	document.head.appendChild(awesomeBootstrapCheckboxCss);
-	document.head.appendChild(jqueryQueryBuilderCss);
+  if (!document.head) {
+    await new Promise(resolve => {
+      const check = () => {
+        if (document.head) resolve();
+        else requestAnimationFrame(check);
+      };
+      check();
+    });
+  }
+
+  const awesomeBootstrapCheckboxCss = document.createElement('link');
+  awesomeBootstrapCheckboxCss.rel = 'stylesheet';
+  awesomeBootstrapCheckboxCss.href = await GM.getResourceUrl('awesome-bootstrap-checkbox');
+  const jqueryQueryBuilderCss = document.createElement('link');
+  jqueryQueryBuilderCss.rel = 'stylesheet';
+  jqueryQueryBuilderCss.href = await GM.getResourceUrl('jquery-query-builder');
+  document.head.appendChild(awesomeBootstrapCheckboxCss);
+  document.head.appendChild(jqueryQueryBuilderCss);
 })();
