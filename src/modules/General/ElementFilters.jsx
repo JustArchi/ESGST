aaf6a86a35a81544dd12b528a51a9d5afb4f7c87
SquishedPotatoe <5186124+SquishedPotatoe@users.noreply.github.com>
2026-05-29 15:36:54 -0600

    Update hide pinned giveaways example

--- src/modules/General/ElementFilters.jsx
+++ src/modules/General/ElementFilters.jsx
@@ -27,7 +27,7 @@ class GeneralElementFilters extends Module {
 						</li>
 						<li>
 							To hide the pinned giveaways (the multiple copy giveaways) in the main page, use:{' '}
-							<code>[esgst.giveawaysPath].pinned-giveaways</code>
+							<code>[esgst.giveawaysPath].pinned-giveaways-header, [esgst.giveawaysPath].pinned-giveaways</code>
 						</li>
 					</ul>
 				</ul>
